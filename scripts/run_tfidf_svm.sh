61e395c0d36072fc18e42c42cc7d8dee245394e9
DianeZh <yifanzhang2024@u.northwestern.edu>
2023-05-23 22:18:39 -0500

    able to run

--- scripts/run_tfidf_svm.sh
+++ scripts/run_tfidf_svm.sh
@@ -2,4 +2,4 @@ DATASET='eurlex'
 TASK_TYPE='multi_label'
 N_CLASSES=100
 
-python models/tfidf_svm.py --dataset ${DATASET} --task_type ${TASK_TYPE} --n_classes ${N_CLASSES}
+python experiments/models/tfidf_svm.py --dataset ${DATASET} --task_type ${TASK_TYPE}
